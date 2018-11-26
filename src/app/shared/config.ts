854f4e610678833f7e75832871f379373fec331b
Khen Solomon Lethil <khensolomon@gmail.com>
2018-11-26 01:55:52 +0100

    GG!

--- src/app/shared/config.ts
+++ src/app/shared/config.ts
@@ -19,8 +19,8 @@ export class Config {
   ];
 }
 export class Environment {
-  // static apiUrl = "https://baas.kinvey.com/";
-  // static appKey = "kid_HyHoT_REf";
-  // static authHeader = "Basic a2lkX0h5SG9UX1JFZjo1MTkxMDJlZWFhMzQ0MzMyODFjN2MyODM3MGQ5OTIzMQ";
+  // static apiUrl = "";
+  // static appKey = "";
+  // static authHeader = "";
   // static token = "";
 }

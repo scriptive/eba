fa85a75b5fb8f96bb163d0f902e889386b37aff4
Khen Solomon Lethil <khensolomon@gmail.com>
2018-11-26 01:50:45 +0100

    Remove compiled files
    Mostly *.js

--- src/app/shared/status-bar-util.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function testingShared() {
-    console.log('testingShared->ok');
-}
-exports.testingShared = testingShared;
